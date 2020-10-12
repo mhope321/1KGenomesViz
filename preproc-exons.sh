49ead1613a06a693b8b978397626517fb1cb1032
Matt Hope <mhope321@gmail.com>
2020-10-12 11:27:56 -0500

    started pre-processing for just exons, preproc-exons.sh

---
+++ preproc-exons.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#Preprocessing script to get all gene names and positions in the human genome
+
+gzip -cd Homo_sapiens.GRCh38.101.chr.gtf.gz | 
+awk 'BEGIN {OFS=","}
+	{
+	if($3=="exon")
+	{
+	print $1, $4, $5, $10
+	}
+     }' | 
+sort | 
+uniq > exons.csv
