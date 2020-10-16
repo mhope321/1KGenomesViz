30e7f6e9ec85854f63b064995906cfa4191c42db
Matt Hope <mhope321@gmail.com>
2020-10-15 21:56:11 -0500

    Wrote function to parse multi json. Still needs work.

--- dbSNP-query.R
+++ dbSNP-query.R
@@ -13,7 +13,28 @@ variant_json <- fromJSON(variant_text,flatten=TRUE)
 
 library(stringr)
 
+parse_json = function(x)
+{
+  result = list()
+  coords = str_locate_all(x,"refsnp_id")[[1]][,1]
+  n_records = length(coords)
+  for (i in 1:n_records)
+  {
+    if (i==n_records)
+    {
+      json = str_sub(x,(coords[i]-2),(nchar(x)))
+      result = append(result,json)
+    } else {
+      json = str_sub(x,(coords[i]-2),(coords[i+1]-2))
+      result = append(result,json)
+    }
+  }
+  return(result)
+}
+
 variant_json <- fromJSON(json3,flatten=TRUE)
 json1 = str_sub(variant_text,1,17921)
 json2 = str_sub(variant_text,17922,34696)
 json3 = str_sub(variant_text,34697,51692)
+
+coords = str_locate_all(variant_text,"refsnp_id")
